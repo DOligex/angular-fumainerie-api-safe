c7b2c286e5efa509cccc81eb0b09c1343988d661
Delache <delache33@gmail.com>
2020-01-02 11:24:54 +0100

    feat: validedWitnessRoute

--- src/repository/question.repository.ts
+++ src/repository/question.repository.ts
@@ -3,11 +3,11 @@ import { DbHandler } from './db.handler';
 
 export class QuestionRepository {
 
-    private GET_ALL = 'SELECT * FROM question ';
-    private GET_BY_ID = 'SELECT * FROM question WHERE id = ?;';
-    private POST_BY_ID = 'INSERT INTO question SET ?;';
-    private PUT_BY_ID = 'UPDATE question SET ? WHERE id = ?;';
-    private DEL_BY_ID = 'DELETE FROM question WHERE id = ?;';
+    private GET_ALL = 'SELECT * FROM question join user on user_id = user.id ';
+    private GET_BY_ID = 'SELECT * FROM question join user on user_id = user.id WHERE id = ?;';
+    private POST_BY_ID = 'INSERT INTO question join user on user_id = user.id SET ?;';
+    private PUT_BY_ID = 'UPDATE question join user on user_id = user.id SET ? WHERE id = ?;';
+    private DEL_BY_ID = 'DELETE FROM question join user on user_id = user.id WHERE id = ?;';
 
     private db: DbHandler;
 
