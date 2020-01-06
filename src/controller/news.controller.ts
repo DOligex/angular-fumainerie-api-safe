9063bda679cfbeec7db7ab7b1ccf108e5fe35f99
Julie Hellec <hellecjulie@gmail.com>
2020-01-06 15:54:50 +0100

    Feat: newsController with commonController

--- src/controller/news.controller.ts
+++ src/controller/news.controller.ts
@@ -1,22 +1,14 @@
-import { News } from '../models/news';
 import { NewsService } from '../services/news.service';
-import express, { Application } from 'express';
-import { AbstractController } from '../core/abstract.controller';
+import { Application } from 'express';
+import { commonController } from 'src/core/common.controller';
 
-export class NewsController extends AbstractController<News> {
-    protected route!: string;
-    service = new NewsService();
+export const NewsController = (app: Application) => {
+    const service = new NewsService();
+    const router = commonController(app, service);
 
-    constructor(app: Application) {
-        super('news', app);
-    }
-    protected setupAdditionalRoute(router: express.Router): void | express.Router {
+    router.get('/specificroute', (req, res) => {
+        res.send('totot');
+    });
 
-        router.get('/specificroute', (req, res) => {
-            res.send('totot');
-        });
-
-        return router;
-                }
-
-}
+    app.use('/news', router);
+};
