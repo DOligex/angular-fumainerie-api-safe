6ddb755bf74bacbd2658b573e8e37fa953d7741d
Julie Hellec <hellecjulie@gmail.com>
2020-01-06 17:46:24 +0100

    Feat:  router arg in commonController, set getByDate

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,4 +1,3 @@
-
 import { Event } from './../models/event';
 import { EventRepository } from '../repository/event.repository';
 import { AbstractService } from '../core/abstract.service';
@@ -8,8 +7,8 @@ export class EventService extends AbstractService<Event> {
     repository = new EventRepository();
 
     // recherche évenement par mot clé
-    // async getBySearch(word: string) {
-    //     const search = await this.repository.searchEvent(word);
-    //     return search;
-    // }
+    async getByDate() {
+        const search = await this.repository.getEventByDate();
+        return search;
+    }
 }
