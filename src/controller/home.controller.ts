af6d6a553125c0f4e99d1f4024de5204780330df
Julie Hellec <hellecjulie@gmail.com>
2020-01-06 16:07:48 +0100

    Feat: homeController with commonController

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -2,6 +2,9 @@ import { HomeService } from '../services/home.service';
 import { Application } from 'express';
 import { commonController } from 'src/core/common.controller';
 
+// Le controller vous servira à réceptionner les requêtes associées au foyer
+// @param app l'application express
+
 export const HomeController = (app: Application) => {
     const service = new HomeService();
     const router = commonController(app, service);
