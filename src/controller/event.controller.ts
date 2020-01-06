9c7032402939432b2709ae4c40e401edad110e50
Julie Hellec <hellecjulie@gmail.com>
2020-01-06 16:08:45 +0100

    Feat: eventController with commonController

--- src/controller/event.controller.ts
+++ src/controller/event.controller.ts
@@ -2,6 +2,9 @@ import { Application } from 'express';
 import { commonController } from '../core/common.controller';
 import { EventService } from './../services/event.service';
 
+// Le controller vous servira à réceptionner les requêtes associées aux évenements
+// @param app l'application express
+
 export const EventController = (app: Application) => {
     const service = new EventService();
     const router = commonController(app, service);
