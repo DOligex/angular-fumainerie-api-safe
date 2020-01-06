bed6f877ac2d17637a61eb4d952b04fff8fd50c6
Julie Hellec <hellecjulie@gmail.com>
2020-01-06 15:51:19 +0100

    Feat: drainingController with commonController

--- src/controller/draining.controller.ts
+++ src/controller/draining.controller.ts
@@ -1,26 +1,20 @@
 
 import { Draining } from '../models/draining';
 import { DrainingService } from '../services/draining.service';
-import express, { Application } from 'express';
-import { AbstractController } from '../core/abstract.controller';
+import { Application } from 'express';
+import { commonController } from 'src/core/common.controller';
 
-// Le controller vous servira à réceptionner les requêtes associées aux utilisateurs
+// Le controller vous servira à réceptionner les vidanges associées aux utilisateurs
 // @param app l'application express
 
-export class DrainingController extends AbstractController<Draining> {
-    protected route!: string;
-    service = new DrainingService();
+export const DrainingController = (app: Application) => {
+    const service = new DrainingService();
 
-    constructor(app: Application) {
-        super('draining', app );
-    }
+    const router = commonController(app, service);
 
-    protected setupAdditionalRoute(router: express.Router): void | express.Router {
-        // route spécifique à décrire ci-dessous
-        router.get('/specificroute', (req, res) => {
-            res.send('totot');
-        });
+    router.get('/specificroute', (req, res) => {
+        res.send('totot');
+    });
 
-        return router;
-                }
-}
+    app.use('/draining', router);
+};
