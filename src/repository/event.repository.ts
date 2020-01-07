71a64758c49c38d96c11bc123f59327f1d6c6775
Julie Hellec <hellecjulie@gmail.com>
2020-01-07 08:33:59 +0100

    Feat: Get event order by date

--- src/repository/event.repository.ts
+++ src/repository/event.repository.ts
@@ -8,7 +8,7 @@ export class EventRepository extends AbstractRepository<Event> {
     }
 
     // Recherche des événements dont la date est suppérieureau à aujourd'hui
-    private GET_EVENT_BY_DATE_SUP_NOW = 'SELECT * FROM event WHERE date > CURDATE()' ;
+    private GET_EVENT_BY_DATE_SUP_NOW = 'SELECT * FROM event WHERE date > CURDATE() ORDER BY date' ;
 
     async getEventByDate() {
         const result = await this.db.query(this.GET_EVENT_BY_DATE_SUP_NOW);
