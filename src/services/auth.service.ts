1244e0ccb6a75092700d59dc336d08e85ccb1007
Julie Hellec <hellecjulie@gmail.com>
2020-01-09 10:00:37 +0100

    Setup nodemailer

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,6 +3,7 @@ import { User } from '../models/user';
 import { hash, verify } from 'argon2';
 import { sign } from 'jsonwebtoken';
 import { randomBytes } from 'crypto';
+import { createTestAccount, createTransport, getTestMessageUrl } from 'nodemailer';
 
 export class AuthService {
 
@@ -50,5 +51,38 @@ export class AuthService {
 
         return token;
     }
+
+ async nodemailer(token: string, user: User) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  const testAccount = await createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: 'bar@example.com, baz@example.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>', // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
 
 }
