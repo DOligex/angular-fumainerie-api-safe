b3b0c517bf6a2b9813fff0340f8a86becf913faf
Julie Hellec <hellecjulie@gmail.com>
2020-01-09 09:21:10 +0100

    Feat: token service and co

---
+++ src/repository/token.repository.ts
@@ -0,0 +1,10 @@
+import { AbstractRepository } from '../core/abstract.repository';
+import { Token } from '../models/token';
+
+export class TokenRepository extends AbstractRepository<Token> {
+
+    constructor() {
+        super('token');
+
+    }
+}
