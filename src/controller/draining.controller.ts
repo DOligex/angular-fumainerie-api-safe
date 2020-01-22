1c7c5049ed3c88cfdf7af620ed90657e658ae082
Adrien Bouissier <adrien.bouissier@gmail.com>
2020-01-22 16:05:26 +0100

    feat:roles  middleware on routes

--- src/controller/draining.controller.ts
+++ src/controller/draining.controller.ts
@@ -2,13 +2,19 @@ import { vidangeurMiddleware } from './../core/vidangeur.middleware';
 import { DrainingService } from '../services/draining.service';
 import { Application, Router } from 'express';
 import { commonController } from '../core/common.controller';
+import jwt = require('express-jwt');
 
 // Le controller vous servira à réceptionner les requetes associées aux vidanges
 // @param app l'application express
 
 export const DrainingController = (app: Application) => {
     const service = new DrainingService();
-    const router = Router();
+    let router = Router();
+
+    if (!process.env.WILD_JWT_SECRET) {
+        throw new Error('Secret is not defined');
+    }
+    router.use(jwt({secret: process.env.WILD_JWT_SECRET}));
 
     router.get('/user/:id', async (req, res) => {
         const userId = parseInt(req.params.id, 10);
@@ -39,8 +45,6 @@ export const DrainingController = (app: Application) => {
             res.status(404).send('L\'id ' + userId + 'n\'a pas été trouvé');
         }
     });
-//////////////////////////////////////////////////////////////
-    // router = commonController(app, service, router);
-////////////////////////////////////////////////////////
+    router = commonController(app, service, router);
     app.use('/draining', router);
 };
