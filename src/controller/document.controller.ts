445049c42531254435a0a4da21c837bbe11a3250
Adrien Bouissier <adrien.bouissier@gmail.com>
2020-01-22 16:34:41 +0100

    feat: specific route get document on homepage

--- src/controller/document.controller.ts
+++ src/controller/document.controller.ts
@@ -13,55 +13,53 @@ let router = Router();
 // @param app l'application express
 
 export const DocumentController = (app: Application) => {
-    const service = new DocumentService();
+  const service = new DocumentService();
 
-    router.get('/accueil', async (req, res, next) => {
-
-      try {
-        const result = await service.getAll();
-        res.send(result);
-    } catch (error) {
-        res.status(404).send('Récupération impossible');
-    }
-
-    });
-
-    if (!process.env.WILD_JWT_SECRET) {
-      throw new Error('Secret is not defined');
-  }
-    router.use(jwt({secret: process.env.WILD_JWT_SECRET}));
-
-    const storage = multer.diskStorage({
-      destination: (req, file, cb ) => {
-        cb(null, env.uploadFolder + '/');
-      },
-      filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + '.pdf' );
-      },
-    });
-    const upload = multer({ storage,
-      fileFilter: (req, file, cb) => {
-        if (!file.originalname.match(/\.(pdf)$/)) {
-          return cb(new Error('Seul les formats PDF sont acceptés'), false);
-        }
-        cb(null, true);
-      },
-    });
-
-    router.post('/file', adminMiddleware, upload.single('file'), async (req, res, next) => {
-      const file = req.file;
-      if (!file) {
-        const error = new Error('Please upload a file');
-        res.sendStatus(400);
-        return next(error);
-      }
-      console.log(req.file);
-      // const document = new Document();
-      req.body.link = req.file.filename;
-      const result = await service.create(req.body);
+  router.get('/accueil', async (req, res, next) => {
+    try {
+      const result = await service.getAll();
       res.send(result);
-    });
+    } catch (error) {
+      res.status(404).send('Récupération impossible');
+    }
+  });
 
-    router = commonController(app, service, router);
-    app.use('/document', router);
-  };
+  if (!process.env.WILD_JWT_SECRET) {
+    throw new Error('Secret is not defined');
+  }
+  router.use(jwt({secret: process.env.WILD_JWT_SECRET}));
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb ) => {
+      cb(null, env.uploadFolder + '/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + '.pdf' );
+    },
+  });
+  const upload = multer({ storage,
+    fileFilter: (req, file, cb) => {
+      if (!file.originalname.match(/\.(pdf)$/)) {
+        return cb(new Error('Seul les formats PDF sont acceptés'), false);
+      }
+      cb(null, true);
+    },
+  });
+
+  router.post('/file', adminMiddleware, upload.single('file'), async (req, res, next) => {
+    const file = req.file;
+    if (!file) {
+      const error = new Error('Please upload a file');
+      res.sendStatus(400);
+      return next(error);
+    }
+    console.log(req.file);
+    // const document = new Document();
+    req.body.link = req.file.filename;
+    const result = await service.create(req.body);
+    res.send(result);
+  });
+
+  router = commonController(app, service, router);
+  app.use('/document', router);
+};
