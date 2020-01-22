8f8ade91facb985dd63b3d45c7b6d4a85b187bd7
Adrien Bouissier <adrien.bouissier@gmail.com>
2020-01-22 11:34:39 +0100

    chore: correction synthaxe 'function' et non 'fonction'

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -58,7 +58,7 @@ export class AuthService {
             throw error;
         }
 
-        const payload = {id: user.id, email: user.email, firstname: user.firstname, accountStatus: user.account_status, fonction: user.fonction};
+        const payload = {id: user.id, email: user.email, firstname: user.firstname, accountStatus: user.account_status, function: user.function};
         if (!process.env.WILD_JWT_SECRET) {
             throw new Error('Server is not correctly configured');
         }
