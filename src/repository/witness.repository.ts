b369c31e1990ae5f82a1a0d8c9d0483a4f810a55
Delache <erique.delacharlerie@gmail.com>
2020-02-02 16:32:36 +0100

    fix witnessRestore

--- src/repository/witness.repository.ts
+++ src/repository/witness.repository.ts
@@ -6,13 +6,9 @@ export class WitnessRepository extends AbstractRepository<Witness> {
     constructor() {
         super('witness');
     }
-
     private GET_VALIDED = 'SELECT * FROM witness JOIN user ON user_id = user.id WHERE status = 1;';
 
-
-    async getValidated(status: number) {
-        // this.db.query(this.GET_VALIDED, [this.GET_VALIDED]) 🤨
-        // Le deuxième paramètre de la fonction est la valeur du "?" heureusmeent il n'y en a pas 🤨
+    async getValidated() {
         const result = await this.db.query(this.GET_VALIDED);
         return result;
     }
