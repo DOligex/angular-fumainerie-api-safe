48aae9d3798aae5e2d4f9c0fafd85bbd25fd02d4
Delache <erique.delacharlerie@gmail.com>
2020-02-03 10:07:30 +0100

    fix: modifWitnessDb

--- src/repository/witness.repository.ts
+++ src/repository/witness.repository.ts
@@ -6,10 +6,10 @@ export class WitnessRepository extends AbstractRepository<Witness> {
     constructor() {
         super('witness');
     }
-    private GET_VALIDED = 'SELECT * FROM witness JOIN user ON user_id = user.id WHERE status = 1;';
+    private GET_VALIDATED = 'SELECT * FROM witness JOIN user ON user_id = user.id WHERE status = 1;';
 
     async getValidated() {
-        const result = await this.db.query(this.GET_VALIDED);
+        const result = await this.db.query(this.GET_VALIDATED);
         return result;
     }
 }
